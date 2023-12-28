9de5250a3a2e6ed42ccc300414abd5e87bfeafd5
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-12-28 08:06:07 -0300

    fixes - content articles - fix the css properties to correct the layout.

--- src/styled/Content.jsx
+++ src/styled/Content.jsx
@@ -3,11 +3,14 @@ import styled from 'styled-components';
 const Content = styled.article`
   overflow-y: auto;
   overflow-x: hidden;
-  width: 100%;
-  height: 88%;
-  background-color: #ffffff15;
+  width: ${ props => props.$width || '100%' };
+  height: ${ props => props.$height || '85%' };
   border-radius: 10px 10px 0 0;
-  margin: 0;
+  margin: 0 auto;
+
+  @media (max-width: 400px) {
+    overflow: hidden;
+  }
 `;
 
 export default Content;
