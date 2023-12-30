b7c4ff995eb19415b0a6f1cb3767df312c663d10
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2023-12-30 10:48:19 -0300

    chore - moves the parameters of navigation control from stacks carousel comp. to stacks context API.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,120 +1,50 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useContext } from 'react';
+import { StacksContext } from '../context/Contexts';
 import { Title2, Title3 } from '../styled/Titles';
 import { CardPicture } from '../styled/Pictures';
 import { SimpleP } from '../styled/Paragraphs';
-import ControlBar from '../styled/ControlBar';
 import Content from '../styled/Content';
-import StacksContainer from '../styled/StacksContainer';
-import { ControlButton } from '../styled/Buttons';
 import { InnerContent } from '../styled/Container';
 import Link from '../styled/Link';
 
-const Carousel = ({ cards, intervalTime }) => {
-  const FIRST_CARD_INDEX = 0;
-  const LAST_CARD_INDEX = cards.length - 1;
-  const [currentCard, setCurrentCard] = useState(FIRST_CARD_INDEX);
-
-  const goToNextCard = useCallback(() => {
-    if (currentCard === LAST_CARD_INDEX) {
-      setCurrentCard(0);
-      return
-    }
-
-    setCurrentCard((currentCard + 1));
-  }, [LAST_CARD_INDEX, currentCard]);
-
-  const goToPreviousCard = () => {
-    if (currentCard === FIRST_CARD_INDEX) {
-      setCurrentCard(LAST_CARD_INDEX);
-      return;
-    };
-
-    setCurrentCard((currentCard - 1));
-  };
-
-  const goToLastCard = () => {
-    setCurrentCard(LAST_CARD_INDEX);
-  };
-
-  const goToFirstCard = () => {
-    setCurrentCard(FIRST_CARD_INDEX);
-  };
-
-  useEffect(() => {
-    const carouselInterval = setInterval(goToNextCard, intervalTime);
-
-    return () => {
-      clearInterval(carouselInterval);
-    };
-  }, [currentCard, goToNextCard, intervalTime]);
+const Carousel = () => {
+  const { stacks, currentStackIndex } = useContext(StacksContext);
 
   return (
-    <StacksContainer>
-      {
-        (cards && cards.length > 0) &&
-          <Content key={cards[currentCard].id}>
-            <InnerContent>
-              <Title2>{ cards[currentCard].title }</Title2>
-              <CardPicture src={ cards[currentCard].imageUrl } alt={ cards[currentCard].title } />
-            </InnerContent>
-            
-            <InnerContent
-              $padding={ '15px 5px 30px 5px' }
-            >
-              <SimpleP $padding={ '0 0 0 10px' } $width={ '95%' }>{ cards[currentCard].description }</SimpleP>
-            </InnerContent>
-            
-            <InnerContent
-              $padding={ '0 5px 15px 5px' }
-            >
-              <Title3>Documentação:</Title3>
-
-              <Link href={ cards[currentCard].stackDocsUrl } target="_blank" rel="noopener noreferrer">
-                <SimpleP $padding={ '0 0 0 10px' }>{ cards[currentCard].stackDocsUrl }</SimpleP>
-              </Link>
-            </InnerContent>
-
-            <InnerContent
-              $padding={ '0 5px 30px 5px' }
-            >
-              <Title3>Página da Ferramenta:</Title3>
-              <Link href={ cards[currentCard].stackUrl } target="_blank" rel="noopener noreferrer">
-                <SimpleP $padding={ '0 0 0 10px' }>{ cards[currentCard].stackUrl }</SimpleP>
-              </Link>
-            </InnerContent>
-          </Content>
-      }
-
-      <ControlBar>
-        <ControlButton
-          type="button"
-          onClick={() => goToFirstCard()}
+    (stacks && stacks.length > 0) &&
+      <Content
+        key={stacks[currentStackIndex].id}
+      >
+        <InnerContent>
+          <Title2>{ stacks[currentStackIndex].title }</Title2>
+          <CardPicture src={ stacks[currentStackIndex].imageUrl } alt={ stacks[currentStackIndex].title } />
+        </InnerContent>
+        
+        <InnerContent
+          $padding={ '15px 5px 30px 5px' }
         >
-          { '|<' }
-        </ControlButton>
-
-        <ControlButton
-          type="button"
-          onClick={() => goToPreviousCard()}
+          <SimpleP $padding={ '0 0 0 10px' } $width={ '95%' }>{ stacks[currentStackIndex].description }</SimpleP>
+        </InnerContent>
+        
+        <InnerContent
+          $padding={ '0 5px 15px 5px' }
         >
-          { '<' }
-        </ControlButton>
+          <Title3>Documentação:</Title3>
 
-        <ControlButton
-          type="button"
-          onClick={() => goToNextCard()}
-        >
-          { '>' }
-        </ControlButton>
+          <Link href={ stacks[currentStackIndex].stackDocsUrl } target="_blank" rel="noopener noreferrer">
+            <SimpleP $padding={ '0 0 0 10px' }>{ stacks[currentStackIndex].stackDocsUrl }</SimpleP>
+          </Link>
+        </InnerContent>
 
-        <ControlButton
-          type="button"
-          onClick={() => goToLastCard()}
+        <InnerContent
+          $padding={ '0 5px 30px 5px' }
         >
-          { '>|' }
-        </ControlButton>
-      </ControlBar>
-    </StacksContainer>
+          <Title3>Página da Ferramenta:</Title3>
+          <Link href={ stacks[currentStackIndex].stackUrl } target="_blank" rel="noopener noreferrer">
+            <SimpleP $padding={ '0 0 0 10px' }>{ stacks[currentStackIndex].stackUrl }</SimpleP>
+          </Link>
+        </InnerContent>
+      </Content>
   );
 }
 
